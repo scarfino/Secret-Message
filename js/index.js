2fa071e447dae8407befe33c4f65a91577ab5ee2
scarfino <35802058+scarfino@users.noreply.github.com>
2018-05-26 18:28:10 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*
+Transform an array of strings into a new "hidden" message
+@scarfino 2018
+*/
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+secretMessage.push('to', 'Program');
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 5, 'know,')
+
+console.log(secretMessage.join(' '));
